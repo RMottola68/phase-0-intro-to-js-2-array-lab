142b33fefc04ad1dfa3fe58fce6b1f8e6ebd912d
Ryan Mottola <rmottola68@gmail.com>
2022-06-07 19:16:50 -0400

    commit tree 2c07cc24a10a095029c7254a5e4565411ad46357

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+let destructivelyAppendCat = (name) => {
+    return cats.push(name);
+}
+
+let destructivelyPrependCat = (name) => {
+    return cats.unshift(name);
+}
+
+let destructivelyRemoveLastCat = (name) => {
+    cats.pop();
+    return cats;
+}
+
+let destructivelyRemoveFirstCat = (name) => {
+    return cats.shift();
+}
+
+let appendCat = (name) => {
+    let newCats = [...cats, name];
+    return newCats
+}
+
+let prependCat = (name) => {
+    let newCats = [name, ...cats];
+    return newCats
+}
+
+let removeLastCat = (name) => {
+    let newCats = cats.slice(0, cats.length-1);
+    return newCats
+}
+
+let removeFirstCat = (name) => {
+    let newCats = cats.slice(1, cats.length);
+    return newCats
+}
